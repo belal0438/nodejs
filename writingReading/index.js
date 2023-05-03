c35f2bb18acf40f1ceba02d8c23b7a927d5886df
Belal <mohammadvns121@gmail.com>
2023-05-04 01:40:15 +0530

    clean up code

--- writingReading/index.js
+++ writingReading/index.js
@@ -1,63 +1,7 @@
 
 const http = require('http');
-const fs = require('fs');
-const { buffer } = require('stream/consumers');
-const path = require('path');
+const  routes = require('./route');
 
-const server = http.createServer((req, res) => {
-
-
-    if (req.url == '/') {
-
-        const filepath = path.join(__dirname, "message.txt");
-
-        fs.readFile(filepath, { encoding: "utf-8" }, (err, data) => {
-            if (err) { console.log(err) };
-            // console.log(data);
-
-            res.write('<html>');
-            res.write('<head><title>Enter Massage</title></head>');
-            res.write(`<body>${data}</body>`);
-            res.write('<body><form action = "/message"  method ="POST"><input type = "text" name ="message"><button>sbmit</button></form></body>');
-            res.write('</html>');
-            return res.end();
-
-        })
-    }else  if (req.url == '/message' && req.method == "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-
-            // console.log(" chunk  " + chunk);
-
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-
-            const ParseBody = Buffer.concat(body).toString();
-
-            // console.log("ParseBody  " + ParseBody);
-
-            const message = ParseBody.split('=')[1];
-
-            // console.log(" message  " + message);
-
-            fs.writeFileSync('message.txt', message);
-
-        })
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-
-    } else {
-        res.setHeader('Conten-Type', 'text/html')
-        res.write('<html>');
-        res.write('<head><title>Enter Massage</title></head>');
-        res.write('<body><h1>Error 404 : page not found</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
-
-})
+const server = http.createServer(routes);
 
 server.listen(4000, 'localhost');
